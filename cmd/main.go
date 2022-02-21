10753b74ef2e8e4bf233bedb9ae1f6e3430292f4
emirtotic <emirtotic@gmail.com>
2022-02-21 14:10:06 +0100

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+)
+
+func main() {
+
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)})
+
+	if err != nil {
+		return
+	}
+	dynaClient := dynamodb.New(awsSession)
+	lambda.Start(handler)
+
+}
+
+const tableName = "LambdaInGoUser"
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	switch request.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(request, tableName, dynaClient)
+	case "POST":
+		return handlers.CreateUser(request, tableName, dynaClient)
+	case "PUT":
+		return handlers.UpdateUser(request, tableName, dynaClient)
+	case "DELETE":
+		return handlers.DeleteUser(request, tableName, dynaClient)
+	}
+
+}
